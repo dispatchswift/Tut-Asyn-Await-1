923d34a34cacb3d056294ee33f6e401f16e9af44
Amari Duran <jch.duran1@gmail.com>
2022-02-07 08:13:32 -0700

    Initial Commit

---
+++ Tut-Async-Await-1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tut-Async-Await-1
+//
+//  Created by Cleopatra on 2/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tut-Async-Await-1/Tut_Async_Await_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Tut_Async_Await_1App.swift
+//  Tut-Async-Await-1
+//
+//  Created by Cleopatra on 2/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Tut_Async_Await_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
